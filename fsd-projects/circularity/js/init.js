98602c0fba227d899ede3c7e97cf494202c57025
Real-rodrigo <rodrigo.salaspule@jpschools.org>
2025-12-11 16:29:25 +0000

    completed circularity project

--- fsd-projects/circularity/js/init.js
+++ fsd-projects/circularity/js/init.js
@@ -28,74 +28,75 @@ var init = function (window) {
     }
 
     // TODO 3 : Call the drawCircle() function
-  };
 
-  // TODO 7 : Use a loop to create multiple circles
-  for (var i = 0; i < 100; i++) {
-    drawCircle();
-  }
-};
-///////////////////
-// PROGRAM LOGIC //
-///////////////////
+    // TODO 7 : Use a loop to create multiple circles
+    for (var i = 0; i < 100; i++) {
+      drawCircle();
+    }
 
-/* 
+    ///////////////////
+    // PROGRAM LOGIC //
+    ///////////////////
+
+    /* 
         This Function is called 60 times/second, producing 60 frames/second.
         In each frame, for every circle, it should redraw that circle
         and check to see if it has drifted off the screen.         
         */
-function update() {
-  for (var i = 0; i < circles.length; i++) {
-    physikz.updatePosition(circles[i]);
-    game.checkCirclePosition(circles[i]);
-  }
-  // TODO 4 : Update the position of each circle using physikz.updatePosition()
-}
+    function update() {
+      for (var i = 0; i < circles.length; i++) {
+        physikz.updatePosition(circles[i]);
+        game.checkCirclePosition(circles[i]);
+      }
+      // TODO 4 : Update the position of each circle using physikz.updatePosition()
+    }
 
-// TODO 5 : Call game.checkCirclePosition() on your circles
+    // TODO 5 : Call game.checkCirclePosition() on your circles
 
-// TODO 8 / TODO 9 : Iterate over the array
+    // TODO 8 / TODO 9 : Iterate over the array
 
-/* 
+    /* 
         This Function should check the position of a circle that is passed to the 
         Function. If that circle drifts off the screen, this Function should move
         it to the opposite side of the screen.
         */
-function checkCirclePosition(circle) {
-  // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
+    game.checkCirclePosition = function (circle) {
+      // if the circle has gone past the RIGHT side of the screen then place it on the LEFT
 
-  // TODO 6 : YOUR CODE STARTS HERE //////////////////////
-  if (circle.x > canvas.width) {
-    circle.x = 0;
-  }
+      // TODO 6 : YOUR CODE STARTS HERE //////////////////////
+      if (circle.x > canvas.width) {
+        circle.x = 0;
+      }
 
-  if (circle.x < 0) {
-    circle.x = canvas.width;
-  }
+      if (circle.x < 0) {
+        circle.x = canvas.width;
+      }
 
-  if (circle.y < 0) {
-    circle.y = canvas.height;
-  }
+      if (circle.y < 0) {
+        circle.y = canvas.height;
+      }
 
-  if (circle.y > canvas.height) {
-    circle.y = 0;
-  }
-}
-// YOUR TODO 6 CODE ENDS HERE //////////////////////////
+      if (circle.y > canvas.height) {
+        circle.y = 0;
+      }
+    };
+    // YOUR TODO 6 CODE ENDS HERE //////////////////////////
 
-/////////////////////////////////////////////////////////////
-// --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
-/////////////////////////////////////////////////////////////
+    /////////////////////////////////////////////////////////////
+    // --- NO CODE BELOW HERE  --- DO NOT REMOVE THIS CODE --- //
+    /////////////////////////////////////////////////////////////
 
-view.addChild(fps);
-app.addUpdateable(fps);
+    view.addChild(fps);
+    app.addUpdateable(fps);
 
-game.circle = circle;
-game.circles = circles;
-game.drawCircle = drawCircle;
-game.update = update;
+    game.circle = circle;
+    game.circles = circles;
+    game.drawCircle = drawCircle;
+    game.update = update;
 
-app.addUpdateable(window.opspark.game);
+    app.addUpdateable(window.opspark.game);
+  };
+}; 
 
 // DO NOT REMOVE THIS CODE //////////////////////////////////////////////////////
 if (
